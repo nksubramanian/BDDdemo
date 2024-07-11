7cb42802ca432879f5ecf94525aee201b9d750a8
subramanian <subramanian.kumarappan@halliburton.com>
2024-07-11 21:32:49 +0530

    calculator service added

---
+++ CalculatorService/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculatorService
+{
+    public class Calculator
+    {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public double Divide(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+
+    }
+}
